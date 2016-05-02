2a38cc1aaa6a047af905ec09fe5a113637f1cebc
Andrzej Krajniak <andrzej.krajniak@gmail.com>
2016-05-02 12:07:49 +0200

    Added relay command

---
+++ PumConverter/PumConverter.WindowsPhone/RelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PumConverter
+{
+    public class RelayCommand : ICommand
+    {
+        private Func<object, bool> _canExecute;
+        private Action<object> _execute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseExecuteChanged() { CanExecuteChanged?.Invoke(this, EventArgs.Empty); }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
