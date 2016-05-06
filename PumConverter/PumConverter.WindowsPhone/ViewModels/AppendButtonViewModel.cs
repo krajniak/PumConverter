39276c92b776172980e466478bf0bb13ac147bb9
Andrzej Krajniak <andrzej.krajniak@gmail.com>
2016-05-06 23:36:33 +0200

    Added base and append button viewmodels

---
+++ PumConverter/PumConverter.WindowsPhone/ViewModels/AppendButtonViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PumConverter.ViewModels
+{
+    public class AppendButtonViewModel : BaseOnScreenButtonViewModel
+    {
+        private string _appendString;
+
+        public AppendButtonViewModel(UnitConverterViewModel parentViewModel) : base(parentViewModel)
+        {
+
+        }
+
+        public string AppendString { get { return _appendString; } set { _appendString = value; RaisePropertyChanged(); } }
+
+        protected override void Execute()
+        {
+            base.Execute();
+
+            _parentViewModel.EntryValue += AppendString;
+        }
+    }
+}
---
+++ PumConverter/PumConverter.WindowsPhone/ViewModels/BaseOnScreenButtonViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PumConverter.ViewModels
+{
+    public abstract class BaseOnScreenButtonViewModel : BaseViewModel
+    {
+        private string _label;
+        protected UnitConverterViewModel _parentViewModel;
+      
+        public BaseOnScreenButtonViewModel(UnitConverterViewModel parentViewModel)
+        {
+            _parentViewModel = parentViewModel;
+
+            ExecuteCommand = new RelayCommand(_ => Execute(), _ => CanExecute() );
+        }
+        
+        protected virtual void Execute()
+        {
+
+        }
+
+        protected virtual bool CanExecute()
+        {
+            return true;
+        }
+
+        public string Label
+        {
+            get { return _label; }
+            set { _label = value; RaisePropertyChanged(); }
+        }
+
+        public RelayCommand ExecuteCommand { get; set; }
+    }
+}
