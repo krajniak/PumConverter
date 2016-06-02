55069500035a38f6f88a578c810a377c02658dab
Andrzej Krajniak <andrzej.krajniak@gmail.com>
2016-06-02 22:33:15 +0200

    Added unit datamodel

---
+++ PumConverter/PumConverter.WindowsPhone/Model/Units/NamedUnit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PumConverter.Model.Units
+{
+    public class NamedUnit
+    {
+        public UnitSystem System { get; private set; }
+        public UnitCategory Category { get; private set; }
+        public double ToSiMultiplier { get; private set; }
+        public string FullName { get; private set; }
+        public string Abbreviation { get; private set; }
+
+        public NamedUnit(UnitSystem system, UnitCategory category, double toSiMultiplier, string fullName, string abbreviation)
+        {
+            System = system;
+            Category = category;
+            ToSiMultiplier = ToSiMultiplier;
+            FullName = fullName;
+            Abbreviation = abbreviation;
+        }
+
+        public double FromSi(double siValue)
+        {
+            return siValue / ToSiMultiplier;
+        }
+
+        public double ToSi(double inUnitValue)
+        {
+            return inUnitValue * ToSiMultiplier;
+        }
+
+        public string ToString(double value)
+        {
+            return value.ToString("N2") + Abbreviation;
+        }
+    }
+}
