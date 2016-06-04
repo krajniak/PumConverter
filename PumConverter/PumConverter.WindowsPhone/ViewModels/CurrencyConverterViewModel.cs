ce5133c1faa36158a1c495be3fa9c7f1b5bdf23b
Andrzej Krajniak <andrzej.krajniak@gmail.com>
2016-06-04 23:18:50 +0200

    Currency view model updated

--- PumConverter/PumConverter.WindowsPhone/ViewModels/CurrencyConverterViewModel.cs
+++ PumConverter/PumConverter.WindowsPhone/ViewModels/CurrencyConverterViewModel.cs
@@ -19,14 +19,47 @@ namespace PumConverter.ViewModels
                 _entryValue = value;
                 RaisePropertyChanged();
                 ProvidedStringChanged?.Invoke(this, EventArgs.Empty);
+                Update();
             }
         }
+
+        private void Update()
+        {
+            try
+            {
+                var value = decimal.Parse(EntryValue);
+                OutputValue = (value * Model.Currencies.CurrencyDataService.Rates[InputCurrency][OutputCurrency].Rate).ToString();
+            } catch
+            {
+                OutputValue = string.Empty;
+            }
+        }
+
+        private string _outputValue;
+        public string OutputValue
+        {
+            get { return _outputValue; }
+            set
+            {
+                _outputValue = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private List<string> _currencies;
+
+        public List<string> Currencies
+        {
+            get { return _currencies; }
+            set { _currencies = value; RaisePropertyChanged(); }
+        }
+
 
         private string _inputCurrency;
         public string InputCurrency
         {
             get { return _inputCurrency; }
-            set { _inputCurrency = value; RaisePropertyChanged(); }
+            set { _inputCurrency = value; RaisePropertyChanged(); Update(); }
         }
 
         private string _outputCurrency;
@@ -34,7 +67,7 @@ namespace PumConverter.ViewModels
         public string OutputCurrency
         {
             get { return _outputCurrency; }
-            set { _outputCurrency = value; RaisePropertyChanged(); }
+            set { _outputCurrency = value; RaisePropertyChanged(); Update(); }
         }
 
 
@@ -58,6 +91,16 @@ namespace PumConverter.ViewModels
             buttonList.Add(new AppendButtonViewModel(this) { Label = "''", AppendString = "''" });
 
             OnScreenKeyboardButtons = buttonList;
+
+            InitCurrencies();
+        }
+
+        public async void InitCurrencies()
+        {
+            await Model.Currencies.CurrencyDataService.LoadRatesAsync();
+            Currencies = Model.Currencies.CurrencyDataService.Currencies.ToList();
+            InputCurrency = Currencies.First();
+            OutputCurrency = Currencies.Skip(1).First();
         }
         
         public string ProvidedString { get { return EntryValue; } set { EntryValue = value; } }
